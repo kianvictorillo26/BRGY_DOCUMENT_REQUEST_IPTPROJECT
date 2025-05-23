c5405a397d9495d2be2e0efad529e69511cbcad9
kianvictorillo26 <kianvictorillo2gmail.com>
2025-05-24 03:17:56 +0800

    update 05-24-25

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CitizenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/get-users', [UserController::class, 'getUsers']);
+    Route::post('/add-user', [UserController::class, 'addUser']);
+    Route::put('/edit-user/{id}', [UserController::class, 'editUser']);
+    Route::delete('/delete-user/{id}', [UserController::class, 'deleteUser']);
+
+    Route::get('/get-citizens', [CitizenController::class, 'getCitizens']);
+    Route::post('/add-citizen', [CitizenController::class, 'addCitizen']);
+    Route::put('/edit-citizen/{id}', [CitizenController::class, 'editCitizen']);
+    Route::delete('/delete-citizen/{id}', [CitizenControllerr::class, 'deleteCitizen']);
+    
+    Route::post('/logout', [CitizenController::class, 'logout']);
 });
